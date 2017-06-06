8d8de25b94e8db800719fe1fd864fabf59028f04
Ru Chern Chong <me@ruchern.com>
2017-06-07 01:00:09 +0800

    Add setup files
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ shell/misc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -e "Updating miscellaneous configuration files..."
+
+ln -s ~/dotfiles/.hushlogin ~/.hushlogin
---
+++ shell/vim.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo -e "Updating your Vim setup and configuration..."
+
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vim
