2e979d18bb6eb75a2144056b15c8c2070471bff0
Ru Chern Chong <me@ruchern.com>
2017-06-10 17:32:46 +0800

    Update vim
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/vim.sh
+++ shell/vim.sh
@@ -2,7 +2,7 @@
 
 echo -e "Updating your Vim setup and configuration..."
 
-rm -rf ~/.vim
+#rm -rf ~/.vim
 rm -f ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
+#ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
