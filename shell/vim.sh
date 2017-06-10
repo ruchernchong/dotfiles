af2e4c2aa59705ea4f2248308a262422666e6e51
Ru Chern Chong <me@ruchern.com>
2017-06-10 13:15:05 +0800

    Update shell script to force delete before performing symlinks
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/vim.sh
+++ shell/vim.sh
@@ -2,5 +2,7 @@
 
 echo -e "Updating your Vim setup and configuration..."
 
+rm -rf ~/.vim
+rm -f ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
--- shell/zsh.sh
+++ shell/zsh.sh
@@ -2,4 +2,5 @@
 
 echo -e "Updating your Zsh setup and configuration..."
 
+rm -f ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
