25cebeef45fba9b971f66a5dc7d59d0160ed5f27
Ru Chern Chong <me@ruchern.com>
2017-06-12 00:44:38 +0800

    Update and add custome theme to zsh
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/zshrc.sh
+++ shell/zshrc.sh
@@ -2,5 +2,7 @@
 
 echo -e "Updating your Zsh setup and configuration..."
 
+cp ~/dotfiles/zsh/themes/clean-custom.zsh-theme ~/.oh-my-zsh/themes/clean-custom.zsh-theme
+
 rm -f ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
