f3dbed5985d01937f6ec40325d59177cbb549082
Ru Chern Chong <me@ruchern.com>
2018-01-22 02:05:17 +0800

    Fix some text and add duration of script
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Start the timer
+echo "Starting script\n"
 timerStart=$(date +%s)
 
 echo "------------------------------------------------"
-echo -e $COL_YELLOW"Setting up your macOS development environment..."$COL_RESET
+echo -e $COL_YELLOW"Setting up your macOS/Linux development environment..."$COL_RESET
 echo "------------------------------------------------"
 
 ##############################################################################
@@ -24,5 +24,12 @@ for file in $SHELL_FILES; do
     ./$file
 done
 
-# Stop the timer
 timerStop=$(date +%s)
+
+duration=$(expr $timerStop - $timerStart)
+
+if [ $duration -lt 60 ]; then
+    echo "Time taken: $(($duration % 60)) seconds\n"
+else
+    echo "Time taken: $(($duration / 60)) minutes and $(($duration % 60)) seconds\n"
+fi 
--- travis.sh
+++ travis.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Start the timer
+echo "Starting script\n"
 timerStart=$(date +%s)
 
 echo "------------------------------------------------"
-echo -e $COL_YELLOW"Setting up your system development environment..."$COL_RESET
+echo -e $COL_YELLOW"Setting up your macOS/Linux development environment..."$COL_RESET
 echo "------------------------------------------------"
 
 ###############################################################################
@@ -18,5 +18,12 @@ for file in $SHELL_FILES; do
     ./$file
 done
 
-# Stop the timer
 timerStop=$(date +%s)
+
+duration=$(expr $timerStop - $timerStart)
+
+if [ $duration -lt 60 ]; then
+    echo "Time taken: $(($duration % 60)) seconds\n"
+else
+    echo "Time taken: $(($duration / 60)) minutes and $(($duration % 60)) seconds\n"
+fi 
