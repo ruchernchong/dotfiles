dbe972211aa5e800f194766db09fec742fce5ed5
Ru Chern Chong <me@ruchern.com>
2018-10-26 00:35:05 +0800

    Add zsh spaceship theme
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ zsh/spaceship-theme.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
