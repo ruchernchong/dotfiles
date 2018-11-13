c8ccbca6fc7263e4933dd8b17088ee0fdb2930b7
Ru Chern Chong <me@ruchern.com>
2018-11-13 20:15:57 +0800

    Update script to symlink .gitignore
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/misc.sh
+++ shell/misc.sh
@@ -2,8 +2,10 @@
 
 echo -e "Updating miscellaneous configuration files..."
 
-
 if [[ "$OSTYPE" != 'linux-gnu' ]]; then
 rm -f ~/.hushlogin
 ln -s ~/dotfiles/.hushlogin ~/.hushlogin
 fi
+
+rm -f ~/.gitignore
+ln -s ~/dotfiles/.gitignore ~/.gitignore
