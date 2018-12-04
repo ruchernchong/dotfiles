3671a654d56fd59e8edb1435a6c661c32253d2c9
Ru Chern Chong <me@ruchern.com>
2018-12-05 00:34:13 +0800

    Replace powerline fonts with nerd fonts
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/nerd-fonts.sh
+++ shell/nerd-fonts.sh
@@ -2,7 +2,10 @@
 
 pwd=$(pwd)
 
-echo -e "Installing Powerline Fonts"
+echo -e "Installing Nerd Fonts"
+
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
 
 cd /tmp
 git clone https://github.com/powerline/fonts.git --depth=1
