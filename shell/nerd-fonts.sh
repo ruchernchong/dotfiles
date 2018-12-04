86d2cbbb97ba36fa62e3060c18845d65a5e2531a
Ru Chern Chong <me@ruchern.com>
2018-12-05 01:19:52 +0800

    Remove installation of Powerline fonts
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- shell/nerd-fonts.sh
+++ shell/nerd-fonts.sh
@@ -6,11 +6,3 @@ echo -e "Installing Nerd Fonts"
 
 brew tap caskroom/fonts
 brew cask install font-hack-nerd-font
-
-cd /tmp
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
-cd $pwd
