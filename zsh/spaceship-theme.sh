4349cdd12f019ce96b5dcf82327679f57186be81
Ru Chern Chong <hello@ruchern.net>
2019-08-13 23:47:22 +0800

    Remove unused themes

--- zsh/spaceship-theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
