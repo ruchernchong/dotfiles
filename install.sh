2f67105aefa897fc18378df82d9c134f6c725de8
Ru Chern Chong <hello@ruchern.net>
2020-05-31 15:34:24 +0800

    Clean up

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v git)" ]; then
   exit 1
 fi
 
-git clone https://github.com/ruchern/dotfiles
+git clone https://github.com/ruchern/dotfiles ~/dotfiles
 cd ~/dotfiles
 chmod a+x setup.sh
 source setup.sh
--- setup/install-homebrew.sh
+++ setup/install-homebrew.sh
@@ -5,6 +5,6 @@ if ! type brew &> /dev/null; then
 
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else if 
-		echo -e "Homebrew is already installed."
+    echo -e "Homebrew is already installed."
 fi
 
